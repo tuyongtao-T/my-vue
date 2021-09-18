4be95f6aaef6a065087cf36bc4c05182499f98bb
tuyogntao1 <tuyongtao1@jd.com>
2021-09-18 20:13:31 +0800

    学习vue源码的入口文件

--- src/core/index.js
+++ src/core/index.js
@@ -2,7 +2,7 @@
  * @Author: TuYongTao
  * @Date: 2021-08-30 20:19:46
  * @LastEditors: your Name
- * @LastEditTime: 2021-08-30 21:51:34
+ * @LastEditTime: 2021-09-07 19:39:26
  * @Description: 这是核心文件的入口
  */
 import Vue from './instance/index'  // 这是ES6 通过 export default 导出的引入方式
@@ -10,4 +10,3 @@ import Vue from './instance/index'  // 这是ES6 通过 export default 导出的
 import { initGlobalAPI } from './global-api/index'  // 这是通过导出相应的接口
 
 initGlobalAPI(Vue)
-
